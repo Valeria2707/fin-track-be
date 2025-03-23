b9226864f75ef9d8c5ef672ef863343eed220063
Valeria Zavadetska <valeriazavadetska@Mac.lan>
2025-03-23 15:19:48 +0200

    set httpOnly false for cookies

--- src/utils/setAuthCookies.ts
+++ src/utils/setAuthCookies.ts
@@ -10,14 +10,14 @@ const SEVEN_DAYS = 7 * 24 * 60 * 60 * 1000;
 
 export function setAuthCookies(res: Response, tokens: Tokens) {
   res.cookie('accessToken', tokens.accessToken, {
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     sameSite: 'lax',
     maxAge: FIFTEEN_MINUTES,
   });
 
   res.cookie('refreshToken', tokens.refreshtoken, {
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     sameSite: 'lax',
     maxAge: SEVEN_DAYS,
